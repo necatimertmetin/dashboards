f619414900a3694c4b6290f9ae690122f2db4eb7
metrosso <necatimertmetin@gmail>
2024-01-08 03:17:44 +0300

    dashboar continued

--- src/components/admin/dashboard/dashboard.js
+++ src/components/admin/dashboard/dashboard.js
@@ -1,18 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import '../../../assets/css/Dashboard.css'
 import Navbar from "../navbar/navbar";
 import rightArrow from '../../../assets/media/right-arrow.png';
 import eyeIcon from '../../../assets/media/eye.png';
 import TinyLineChart from "../../charts/tinyLineChart";
+import DonutChart from "../../charts/donutChart";
+import preferencesIcon from '../../../assets/media/options.png'
 const Dashboard = () => {
 
 
-    const data = [
-        { name: 'Jan', value: 150 },
-        { name: 'Feb', value: 300 },
-        { name: 'Mar', value: 200 },
-        { name: 'Apr', value: 500 },
-      ];
+    function getRandomValue(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    // Function to generate an array with 20 items, each with a random value
+    function generateRandomDataArray() {
+        const dataArray = [];
+        for (let i = 1; i <= 10; i++) {
+            const value = getRandomValue(100, 800); // Adjust the range as needed
+            const title = value % 2 === 0 ? 'Total View' : 'Price'; // Set title conditionally
+            dataArray.push({ name: `Month ${i}`, value, title });
+        }
+        return dataArray;
+    }
+
+    // Generate 4 arrays
+    const data1 = generateRandomDataArray();
+    const data2 = generateRandomDataArray();
+    const data3 = generateRandomDataArray();
+    const data4 = generateRandomDataArray();
+
+    const chartData = [
+        { name: 'Instagram', value: 400 },
+        { name: 'Youtube', value: 300 },
+        { name: 'Category C', value: 200 },
+        { name: 'Category D', value: 100 },
+    ];
+
+    const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+    const [sortBy, setSortBy] = useState('');
+    const [sortOrder, setSortOrder] = useState('asc');
+
+    const handleSort = (key) => {
+        if (sortBy === key) {
+            setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+        } else {
+            setSortBy(key);
+            setSortOrder('asc');
+        }
+    };
+
+    const handleFilter = () => {
+        // Filter işlevselliği ekleyin
+        console.log('Filter button clicked');
+      };
 
     return (
         <div className="dashboard-container">
@@ -33,59 +74,52 @@ const Dashboard = () => {
                     <div className="dashboard-header-container">
                         <div className="dashboard-content-card">
                             <div class="dashboard-content-card2">
-                                <div className="dashboard-content-card-content">
-                                    <img className="dashboard-content-card-img" src={eyeIcon} alt="eye"/>
-                                    <div className="dashboard-content-card-description">
-                                        <div className="dashboard-content-card-title">
-                                            Total View
-                                        </div>
-                                        <div className="dashboard-content-card-value">
-                                            $4562
-                                        </div>
-                                    </div>
-                                </div>
+                                <TinyLineChart data={data1} strokeColor={"#32CD32"} />
                             </div>
                         </div>
                         <div className="dashboard-content-card">
                             <div class="dashboard-content-card2">
-                                <div className="dashboard-content-card-content-right">
-                                    <TinyLineChart data={data} />
-                                </div>
+                                <TinyLineChart data={data2} strokeColor={"#7862f8"} />
                             </div>
                         </div>
                         <div className="dashboard-content-card">
                             <div class="dashboard-content-card2">
-                                <div className="dashboard-content-card-content">
-                                    <img className="dashboard-content-card-img" src={eyeIcon} alt="eye"/>
-                                    <div className="dashboard-content-card-description">
-                                        <div className="dashboard-content-card-title">
-                                            Total View
-                                        </div>
-                                        <div className="dashboard-content-card-value">
-                                            $4562
-                                        </div>
-                                    </div>
-                                </div>
+                                <TinyLineChart data={data3} strokeColor={"#ff0055"} />
                             </div>
                         </div>
                         <div className="dashboard-content-card">
                             <div class="dashboard-content-card2">
-                                <div className="dashboard-content-card-content">
-                                    <img className="dashboard-content-card-img" src={eyeIcon} alt="eye"/>
-                                    <div className="dashboard-content-card-description">
-                                        <div className="dashboard-content-card-title">
-                                            Total View
-                                        </div>
-                                        <div className="dashboard-content-card-value">
-                                            $4562
-                                        </div>
-                                    </div>
-                                </div>
+                                <TinyLineChart data={data4} strokeColor={"#FFD700"} />
                             </div>
                         </div>
-                        
                     </div>
 
+                    <div className="dashboard-content">
+                        <div className="dashboard-radial-container">
+                            <DonutChart chartData={chartData} colors={colors} />
+
+
+                        </div>
+                        <div className="dashboard-mini-table-container">
+                            <div className="dashboard-mini-table-control-container">
+                                <div className="dashboard-mini-table-title" onClick={() => handleSort('name')}>
+                                    Name {sortBy === 'name' && (sortOrder === 'asc' ? '▲' : '▼')}
+                                </div>
+                                <div className="dashboard-mini-table-title" onClick={() => handleSort('email')}>
+                                    Email {sortBy === 'email' && (sortOrder === 'asc' ? '▲' : '▼')}
+                                </div>
+                                <div className="dashboard-mini-table-title" onClick={handleFilter}>
+                                    Filter
+                                </div>
+                                <div className="dashboard-mini-table-control-wrapper" onClick={() => console.log('Other button clicked')}>
+                                    <img className="dashboard-mini-table-control-img" src={preferencesIcon} alt="settings"/>
+                                </div>
+                            </div>
+                            <div className="dashboard-mini-table">
+                                content
+                            </div>
+                        </div>
+                    </div>
 
 
 
--- src/components/charts/tinyLineChart.js
+++ src/components/charts/tinyLineChart.js
@@ -1,21 +1,42 @@
 import React from "react";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-const TinyLineChart = ({ data }) => {
-    return (
-        <ResponsiveContainer width="100%" height="100%">
-            <LineChart
-          data={data}
-          margin={{ top: 20, right: 20, left: 20, bottom: 20 }}
-        >
-          <Tooltip
-            formatter={(value) => [value, 'Your Custom Unit']}
-          />
-          <Legend />
-          <Line type="monotone" dataKey="value" stroke="#8884d8" strokeWidth={3} activeDot={{ r: 3 }} />
-        </LineChart>
-        </ResponsiveContainer>
-    );
+const TinyLineChart = ({ data, strokeColor }) => {
+
+  const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+      const dataPoint = payload[0].payload; // Extracting the data point
+      return (
+        <div className="custom-tooltip">
+          <p className="label">{dataPoint.name}</p>
+          <p className="data">{dataPoint.value}</p>
+        </div>
+      );
+    }
+
+    return null;
+  };
+
+  return (
+    <ResponsiveContainer>
+      <LineChart
+        data={data}
+        margin={{ top: 20, right: 20, left: 20, bottom: 20 }}
+        style={{ cursor: 'pointer' }}
+      >
+        <Tooltip content={<CustomTooltip />} />
+        <Legend />
+        <Line
+          name={data[0].title}
+          type="monotone"
+          dataKey="value"
+          stroke={ strokeColor||"#8884d8"}
+          strokeWidth={3}
+          activeDot={{ r: 3, isAnimationActive: false }} // Disable vertical lines on hover
+        />
+      </LineChart>
+    </ResponsiveContainer>
+  );
 };
 
 export default TinyLineChart;
